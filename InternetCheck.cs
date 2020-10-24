447dc0771d7d9f2874c5fa98914188386ef2f306
Ian Richards <fatigor0@gmail.com>
2020-10-24 18:38:02 +0200

    Added connection check

---
+++ InternetCheck.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+
+namespace InternetMonitor
+{
+
+    public class InternetCheck
+    {
+        public static bool CheckForInternetConnection()
+        {
+            try
+            {
+                using (var client = new WebClient())
+                using (client.OpenRead("http://google.com/generate_204"))
+                    return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ namespace InternetMonitor
         [STAThread]
         static void Main()
         {
+            var check = InternetCheck.CheckForInternetConnection();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
